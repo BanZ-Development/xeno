238b3d1f589d4c91e3e938d5c82c5a8ffa6c9fbd
jackgimre <66290453+jackgimre@users.noreply.github.com>
2021-01-16 22:48:08 -0600

    nedb

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const Datastore = require('nedb');
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 function adminLogin() {
     let password = document.getElementById('passwordInput').value;
     if(password == 'password') {
